cd292e825d982b1cb72596f2169d04b19c545e1b
Fernando Nielson <appsons.business@gmail.com>
2023-04-18 12:01:11 +0200

    Iteración en Arrays de Selects

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  categorias: any[] = [
+    {value: 'general', nombre: 'General' },
+    {value: 'business', nombre: 'Negocios' },
+    {value: 'entertainment', nombre: 'Entretenimiento' },
+    {value: 'health', nombre: 'Salud' },
+    {value: 'science', nombre: 'Ciencia' },
+    {value: 'sports', nombre: 'Deportes' },
+    {value: 'technology', nombre: 'Tecnología' },
+  ];
+
+  paises: any[] = [
+    {value: 'ar', nombre: 'Argentina' },
+    {value: 'br', nombre: 'Brasil' },
+    {value: 'fr', nombre: 'Francia' },
+    {value: 'hu', nombre: 'Hungría' },
+    {value: 'mx', nombre: 'México' },
+    {value: 'gb', nombre: 'Reino Unido' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
